7ca2db72a2f19d8a63373d266b8566613da331e0
Jack Barrett <jbarrett1347@gmail.com>
2021-11-15 15:51:30 -0600

    Fixed a formatting issue

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -2,31 +2,30 @@ import "./UserPage.css";
 import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import ProductCard from '../ProductCardNoDesc/ProductCardNoDesc';
+import ProductCard from "../ProductCardNoDesc/ProductCardNoDesc";
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
   const products = useSelector((store) => store.products);
 
-  console.log("products", products);
   return (
     <>
       <div>
         <h2 className="welcomeText">Welcome, {user.username}!</h2>
       </div>
-      {products <= 0 ? (
-        <p>
-          It looks Like you don't have any favorite devices!
-          <Link to="/info">here</Link> to browse.
-        </p>
-      ) : (
+      {products ? (
         <div>
           {products?.map((product) => {
-            console.log(product);
             return <p>{product.product_name}</p>;
           })}
         </div>
+      ) : (
+        <p className="emptyDevicesText">
+          <br />
+          It looks Like you don't have any favorite devices! Click{" "}
+          <Link to="/info">here</Link> to browse.
+        </p>
       )}
     </>
   );
