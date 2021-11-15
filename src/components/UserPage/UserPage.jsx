4069daa0aeff5c9c4a048385fa956307a3cc37e2
Jack Barrett <jbarrett1347@gmail.com>
2021-11-15 14:46:37 -0600

    Removed a change

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -9,9 +9,9 @@ function UserPage() {
   return (
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
+
       <LogOutButton className="btn" />
-      <ProductCard />
+      {/* <ProductCard /> */}
     </div>
   );
 }
