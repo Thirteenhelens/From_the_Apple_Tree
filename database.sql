2b379b122de764c66d827cca5e08320c05de66c3
Jack Barrett <jbarrett1347@gmail.com>
2021-11-15 11:45:31 -0600

    Added databse connection

--- database.sql
+++ database.sql
@@ -1,4 +1,3 @@
-
 -- USER is a reserved keyword with Postgres
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
@@ -6,5 +5,6 @@
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "clearance_level" SERIAL
 );
