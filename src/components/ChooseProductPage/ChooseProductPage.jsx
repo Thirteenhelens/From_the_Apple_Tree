75327ecee9975c65080c832fc888189383cebaa5
Jack Barrett <jbarrett1347@gmail.com>
2021-11-21 13:30:05 -0600

    Added choose products page with dropdown menu functional for product cards

--- src/components/ChooseProductPage/ChooseProductPage.jsx
+++ src/components/ChooseProductPage/ChooseProductPage.jsx
@@ -1,7 +1,39 @@
+import React from "react";
+import "./ChooseProductPage.css";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import { useSelector } from "react-redux";
+import ProductCardWithDesc from "../ProductCardWithcDesc/ProductCardWithDesc";
+import CompareDevicesButton from "../CompareDevicesButton/CompareDevicesButton";
+
 function ChooseProductPage() {
+  const user = useSelector((store) => store.user);
+  const products = useSelector((store) => store.products);
+
   return (
     <div className="container">
-      <p>ChooseProductPage</p>
+      <h1>Choose Devices</h1>
+      <div className="compareButton">
+        <CompareDevicesButton />
+      </div>
+
+      <Box sx={{ flexGrow: 1, m: 2 }}>
+        <Grid
+          container
+          spacing={2}
+          direction="row"
+          alignItems="flex-start"
+          justifyContent="flex-start"
+        >
+          {products?.map((product) => {
+            return (
+              <Grid item xs={4} key={product.id}>
+                <ProductCardWithDesc product={product} key={product.id} />
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Box>
     </div>
   );
 }
