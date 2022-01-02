809b49ed288359f387802781e33741feaed674cc
Jack Barrett <jbarrett1347@gmail.com>
2022-01-02 17:47:24 -0600

    Added mobile rendering to about page and admin add product

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,35 +1,42 @@
 import "./AboutPage.css";
 import React from "react";
-import { Paper, Typography } from "@mui/material";
+import { Grid, Paper, Typography } from "@mui/material";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
 function AboutPage() {
   return (
-    <div>
-      <h1>A bit about this app</h1>
-      <Paper elevation={4} sx={{ m: 3 }}>
-        <Typography sx={{ p: 2 }}>
-          Made by: Jack B.
-          <br />
-          <br />
-          From the Apple Tree is an application aimed at the non-tech-savvy
-          Apple user on the hunt for a new computer. With an account, a user can
-          peruse Apple’s product lineup through a simple table view, or
-          recommend devices through a questionnaire. Users also have the ability
-          to compare two devices, and ‘favorite’ ones that they are interested
-          in to view or compare later on. This app helps the user navigate the
-          confusing world of buying a computer and can help save them big in the
-          long run.
-          <br />
-          <br />
-          With the help of my friends at Prime, My instructors, Family, and my
-          partner, Emile.
-          <br />
-          <br />
-          Thanks to you all <FavoriteBorderIcon />
-        </Typography>
-      </Paper>
-    </div>
+      <Grid container>
+        {/* Start Container */}
+        <Grid item xs={12}>
+          <h1>A bit about this app</h1>
+        </Grid>
+        {/* End grid item #1 */}
+        <Grid item xs={12}>
+          <Paper elevation={4} sx={{ m: 3 }}>
+            <Typography sx={{ p: 2 }}>
+              Made by: Jack B.
+              <br />
+              <br />
+              From the Apple Tree is an application aimed at the non-tech-savvy
+              Apple user on the hunt for a new computer. With an account, a user
+              can peruse Apple’s product lineup through a simple table view, or
+              recommend devices through a questionnaire. Users also have the
+              ability to compare two devices, and ‘favorite’ ones that they are
+              interested in to view or compare later on. This app helps the user
+              navigate the confusing world of buying a computer and can help
+              save them big in the long run.
+              <br />
+              <br />
+              With the help of my friends at Prime, My instructors, Family, and
+              my partner, Emile.
+              <br />
+              <br />
+              Thanks to you all <FavoriteBorderIcon />
+            </Typography>
+          </Paper>
+        </Grid>
+        {/* End grid item #2 */}
+      </Grid>
   );
 }
 
--- src/components/AdminAddProductPage/AdminAddProductPage.jsx
+++ src/components/AdminAddProductPage/AdminAddProductPage.jsx
@@ -1,6 +1,5 @@
 // MUI IMPORTS
 import {
-  Box,
   Grid,
   Switch,
   Button,
@@ -12,6 +11,7 @@ import { useState } from "react";
 import "./AdminAddProductPage.css";
 import { useHistory } from "react-router";
 import { useDispatch } from "react-redux";
+import { isMobile } from "react-device-detect";
 
 function AdminAddProductPage() {
   const history = useHistory();
@@ -41,11 +41,17 @@ function AdminAddProductPage() {
   });
 
   return (
-    <div className="inputs">
-      <h1>Add a new product</h1>
-
-      <Box sx={{ ml: 35, mr: 10 }}>
-        <div className="buttonContainer">
+    <>
+      <Grid container spacing={2}>
+        {/*  */}
+        <Grid item xs={12}>
+          <h1>Add a new product</h1>
+        </Grid>
+      </Grid>
+      {/*  */}
+      <Grid container>
+        <Grid item xs={2} md={8} />
+        <Grid item xs={10} md={4}>
           <Button
             sx={{ mr: 1 }}
             type="submit"
@@ -67,328 +73,331 @@ function AdminAddProductPage() {
           >
             Cancel
           </Button>
-        </div>
-        <Grid container spacing={2}>
-          <Grid item xs={4} />
-
-          <Grid item xs={4}>
-            <h3>Name</h3>
-            <TextField
-              label="Name"
-              color="secondary"
-              variant="standard"
-              id="standard-basic"
-              value={newProduct.name}
-              onChange={(e) =>
-                setNewProduct({ ...newProduct, name: e.target.value })
-              }
-            />
-          </Grid>
-
-          <Grid item xs={4} />
-
-          <Grid item xs={4}>
-            <h3>Price</h3>
-            <TextField
-              label="Price"
-              color="secondary"
-              variant="standard"
-              id="standard-basic"
-              value={newProduct.price}
-              onChange={(e) =>
-                setNewProduct({ ...newProduct, price: e.target.value })
-              }
-            />
-          </Grid>
-
-          <Grid item xs={4}>
-            <h3>Image url</h3>
-            <TextField
-              multiline
-              label="URL"
-              maxRows={3}
-              color="secondary"
-              variant="standard"
-              id="standard-basic"
-              value={newProduct.image_URL}
-              onChange={(e) =>
-                setNewProduct({ ...newProduct, image_URL: e.target.value })
-              }
-            />
-          </Grid>
-
-          <Grid item xs={4}>
-            <h3>Description</h3>
-            <TextField
-              multiline
-              maxRows={3}
-              color="secondary"
-              variant="standard"
-              id="standard-basic"
-              label="Description"
-              value={newProduct.Description}
-              onChange={(e) =>
-                setNewProduct({ ...newProduct, Description: e.target.value })
-              }
-            />
-          </Grid>
-
-          <Grid item xs={4}>
-            <h3>CPU</h3>
-            <TextField
-              label="CPU Info"
-              color="secondary"
-              variant="standard"
-              id="standard-basic"
-              value={newProduct.cpu_txt}
-              onChange={(e) =>
-                setNewProduct({ ...newProduct, cpu_txt: e.target.value })
-              }
-            />
-          </Grid>
-
-          <Grid item xs={4}>
-            <h3>HDMI</h3>
-            <TextField
-              label="HDMI Info"
-              color="secondary"
-              variant="standard"
-              id="standard-basic"
-              value={newProduct.hdmi_txt}
-              onChange={(e) =>
-                setNewProduct({ ...newProduct, hdmi_txt: e.target.value })
-              }
-            />
-            <br />
-            <FormControlLabel
-              control={
-                <Switch
-                  name="hdmi"
-                  color="secondary"
-                  checked={newProduct.hdmi}
-                  onChange={() =>
-                    setNewProduct({ ...newProduct, hdmi: !newProduct.hdmi })
-                  }
-                />
-              }
-              label="Product has this"
-            />
-          </Grid>
-
-          <Grid item xs={4}>
-            <h3>USBC</h3>
-            <TextField
-              label="USBC Info"
-              color="secondary"
-              variant="standard"
-              id="standard-basic"
-              value={newProduct.usbc_txt}
-              onChange={(e) =>
-                setNewProduct({ ...newProduct, usbc_txt: e.target.value })
-              }
-            />
-            <br />
-            <FormControlLabel
-              control={
-                <Switch
-                  name="usbc"
-                  color="secondary"
-                  checked={newProduct.usbc}
-                  onChange={() =>
-                    setNewProduct({ ...newProduct, usbc: !newProduct.usbc })
-                  }
-                />
-              }
-              label="Product has this"
-            />
-          </Grid>
-
-          <Grid item xs={4}>
-            <h3>Display</h3>
-            <TextField
-              color="secondary"
-              variant="standard"
-              id="standard-basic"
-              label="Display Info"
-              value={newProduct.display_txt}
-              onChange={(e) =>
-                setNewProduct({ ...newProduct, display_txt: e.target.value })
-              }
-            />
-            <br />
-            <FormControlLabel
-              control={
-                <Switch
-                  name="Display"
-                  color="secondary"
-                  checked={newProduct.display}
-                  onChange={() =>
-                    setNewProduct({
-                      ...newProduct,
-                      display: !newProduct.display,
-                    })
-                  }
-                />
-              }
-              label="Product has this"
-            />
-          </Grid>
-
-          <Grid item xs={4}>
-            <h3>Camera</h3>
-            <TextField
-              color="secondary"
-              variant="standard"
-              id="standard-basic"
-              label="Camera Info"
-              value={newProduct.camera_txt}
-              onChange={(e) =>
-                setNewProduct({ ...newProduct, camera_txt: e.target.value })
-              }
-            />
-            <br />
-            <FormControlLabel
-              control={
-                <Switch
-                  name="camera"
-                  color="secondary"
-                  checked={newProduct.camera}
-                  onChange={() =>
-                    setNewProduct({
-                      ...newProduct,
-                      camera: !newProduct.camera,
-                    })
-                  }
-                />
-              }
-              label="Product has this"
-            />
-          </Grid>
-
-          <Grid item xs={4}>
-            <h3>GPU</h3>
-            <TextField
-              label="GPU Info"
-              color="secondary"
-              variant="standard"
-              id="standard-basic"
-              value={newProduct.gpu_txt}
-              onChange={(e) =>
-                setNewProduct({ ...newProduct, gpu_txt: e.target.value })
-              }
-            />
-          </Grid>
-
-          <Grid item xs={4}>
-            <h3>RAM</h3>
-            <TextField
-              label="RAM Info"
-              color="secondary"
-              variant="standard"
-              id="standard-basic"
-              value={newProduct.ram_txt}
-              onChange={(e) =>
-                setNewProduct({ ...newProduct, ram_txt: e.target.value })
-              }
-            />
-          </Grid>
-
-          <Grid item xs={4}>
-            <h3>Storage</h3>
-            <TextField
-              color="secondary"
-              variant="standard"
-              id="standard-basic"
-              label="Storage Info"
-              value={newProduct.storage_TXT}
-              onChange={(e) =>
-                setNewProduct({ ...newProduct, storage_TXT: e.target.value })
-              }
-            />
-          </Grid>
-
-          <Grid item xs={4}>
-            <h3>FingerPrint Sensor</h3>
-            <FormControlLabel
-              control={
-                <Switch
-                  color="secondary"
-                  name="fingerprint"
-                  checked={newProduct.fingerPrint}
-                  onChange={() =>
-                    setNewProduct({
-                      ...newProduct,
-                      fingerPrint: !newProduct.fingerPrint,
-                    })
-                  }
-                />
-              }
-              label="Product has this"
-            />
-          </Grid>
-
-          <Grid item xs={4}>
-            <h3>Backlit Keyboard</h3>
-            <FormControlLabel
-              control={
-                <Switch
-                  name="backlit"
-                  color="secondary"
-                  checked={newProduct.backlit}
-                  onChange={() =>
-                    setNewProduct({
-                      ...newProduct,
-                      backlit: !newProduct.backlit,
-                    })
-                  }
-                />
-              }
-              label="Product has this"
-            />
-          </Grid>
-
-          <Grid item xs={4}>
-            <h3>Touch Screen</h3>
-            <FormControlLabel
-              control={
-                <Switch
-                  color="secondary"
-                  name="touch screen"
-                  checked={newProduct.touchScreen}
-                  onChange={() =>
-                    setNewProduct({
-                      ...newProduct,
-                      touchScreen: !newProduct.touchScreen,
-                    })
-                  }
-                />
-              }
-              label="Product has this"
-            />
-          </Grid>
-
-          <Grid item xs={4}>
-            <h3>Magsafe</h3>
-            <FormControlLabel
-              control={
-                <Switch
-                  name="magsafe"
-                  color="secondary"
-                  checked={newProduct.magsafe}
-                  onChange={() =>
-                    setNewProduct({
-                      ...newProduct,
-                      magsafe: !newProduct.magsafe,
-                    })
-                  }
-                />
-              }
-              label="Product has this"
-            />
-          </Grid>
         </Grid>
-      </Box>
-    </div>
+      </Grid>
+      {/*  */}
+
+      <Grid container spacing={2} sx={!isMobile && { ml: 15 }}>
+        <Grid item xs={4} />
+        {/*  */}
+
+        <Grid item xs={4}>
+          <h3>Name</h3>
+          <TextField
+            label="Name"
+            color="secondary"
+            variant="standard"
+            id="standard-basic"
+            value={newProduct.name}
+            onChange={(e) =>
+              setNewProduct({ ...newProduct, name: e.target.value })
+            }
+          />
+        </Grid>
+
+        <Grid item xs={4} />
+
+        <Grid item xs={4}>
+          <h3>Price</h3>
+          <TextField
+            label="Price"
+            color="secondary"
+            variant="standard"
+            id="standard-basic"
+            value={newProduct.price}
+            onChange={(e) =>
+              setNewProduct({ ...newProduct, price: e.target.value })
+            }
+          />
+        </Grid>
+
+        <Grid item xs={4}>
+          <h3>Image url</h3>
+          <TextField
+            multiline
+            label="URL"
+            maxRows={3}
+            color="secondary"
+            variant="standard"
+            id="standard-basic"
+            value={newProduct.image_URL}
+            onChange={(e) =>
+              setNewProduct({ ...newProduct, image_URL: e.target.value })
+            }
+          />
+        </Grid>
+
+        <Grid item xs={4}>
+          <h3>Description</h3>
+          <TextField
+            multiline
+            maxRows={3}
+            color="secondary"
+            variant="standard"
+            id="standard-basic"
+            label="Description"
+            value={newProduct.Description}
+            onChange={(e) =>
+              setNewProduct({ ...newProduct, Description: e.target.value })
+            }
+          />
+        </Grid>
+
+        <Grid item xs={4}>
+          <h3>CPU</h3>
+          <TextField
+            label="CPU Info"
+            color="secondary"
+            variant="standard"
+            id="standard-basic"
+            value={newProduct.cpu_txt}
+            onChange={(e) =>
+              setNewProduct({ ...newProduct, cpu_txt: e.target.value })
+            }
+          />
+        </Grid>
+
+        <Grid item xs={4}>
+          <h3>HDMI</h3>
+          <TextField
+            label="HDMI Info"
+            color="secondary"
+            variant="standard"
+            id="standard-basic"
+            value={newProduct.hdmi_txt}
+            onChange={(e) =>
+              setNewProduct({ ...newProduct, hdmi_txt: e.target.value })
+            }
+          />
+          <br />
+          <FormControlLabel
+            control={
+              <Switch
+                name="hdmi"
+                color="secondary"
+                checked={newProduct.hdmi}
+                onChange={() =>
+                  setNewProduct({ ...newProduct, hdmi: !newProduct.hdmi })
+                }
+              />
+            }
+            label="Product has this"
+          />
+        </Grid>
+
+        <Grid item xs={4}>
+          <h3>USBC</h3>
+          <TextField
+            label="USBC Info"
+            color="secondary"
+            variant="standard"
+            id="standard-basic"
+            value={newProduct.usbc_txt}
+            onChange={(e) =>
+              setNewProduct({ ...newProduct, usbc_txt: e.target.value })
+            }
+          />
+          <br />
+          <FormControlLabel
+            control={
+              <Switch
+                name="usbc"
+                color="secondary"
+                checked={newProduct.usbc}
+                onChange={() =>
+                  setNewProduct({ ...newProduct, usbc: !newProduct.usbc })
+                }
+              />
+            }
+            label="Product has this"
+          />
+        </Grid>
+
+        <Grid item xs={4}>
+          <h3>Display</h3>
+          <TextField
+            color="secondary"
+            variant="standard"
+            id="standard-basic"
+            label="Display Info"
+            value={newProduct.display_txt}
+            onChange={(e) =>
+              setNewProduct({ ...newProduct, display_txt: e.target.value })
+            }
+          />
+          <br />
+          <FormControlLabel
+            control={
+              <Switch
+                name="Display"
+                color="secondary"
+                checked={newProduct.display}
+                onChange={() =>
+                  setNewProduct({
+                    ...newProduct,
+                    display: !newProduct.display,
+                  })
+                }
+              />
+            }
+            label="Product has this"
+          />
+        </Grid>
+
+        <Grid item xs={4}>
+          <h3>Camera</h3>
+          <TextField
+            color="secondary"
+            variant="standard"
+            id="standard-basic"
+            label="Camera Info"
+            value={newProduct.camera_txt}
+            onChange={(e) =>
+              setNewProduct({ ...newProduct, camera_txt: e.target.value })
+            }
+          />
+          <br />
+          <FormControlLabel
+            control={
+              <Switch
+                name="camera"
+                color="secondary"
+                checked={newProduct.camera}
+                onChange={() =>
+                  setNewProduct({
+                    ...newProduct,
+                    camera: !newProduct.camera,
+                  })
+                }
+              />
+            }
+            label="Product has this"
+          />
+        </Grid>
+
+        <Grid item xs={4}>
+          <h3>GPU</h3>
+          <TextField
+            label="GPU Info"
+            color="secondary"
+            variant="standard"
+            id="standard-basic"
+            value={newProduct.gpu_txt}
+            onChange={(e) =>
+              setNewProduct({ ...newProduct, gpu_txt: e.target.value })
+            }
+          />
+        </Grid>
+
+        <Grid item xs={4}>
+          <h3>RAM</h3>
+          <TextField
+            label="RAM Info"
+            color="secondary"
+            variant="standard"
+            id="standard-basic"
+            value={newProduct.ram_txt}
+            onChange={(e) =>
+              setNewProduct({ ...newProduct, ram_txt: e.target.value })
+            }
+          />
+        </Grid>
+
+        <Grid item xs={4}>
+          <h3>Storage</h3>
+          <TextField
+            color="secondary"
+            variant="standard"
+            id="standard-basic"
+            label="Storage Info"
+            value={newProduct.storage_TXT}
+            onChange={(e) =>
+              setNewProduct({ ...newProduct, storage_TXT: e.target.value })
+            }
+          />
+        </Grid>
+
+        <Grid item xs={4}>
+          <h3>FingerPrint Sensor</h3>
+          <FormControlLabel
+            control={
+              <Switch
+                color="secondary"
+                name="fingerprint"
+                checked={newProduct.fingerPrint}
+                onChange={() =>
+                  setNewProduct({
+                    ...newProduct,
+                    fingerPrint: !newProduct.fingerPrint,
+                  })
+                }
+              />
+            }
+            label="Product has this"
+          />
+        </Grid>
+
+        <Grid item xs={4}>
+          <h3>Backlit Keyboard</h3>
+          <FormControlLabel
+            control={
+              <Switch
+                name="backlit"
+                color="secondary"
+                checked={newProduct.backlit}
+                onChange={() =>
+                  setNewProduct({
+                    ...newProduct,
+                    backlit: !newProduct.backlit,
+                  })
+                }
+              />
+            }
+            label="Product has this"
+          />
+        </Grid>
+
+        <Grid item xs={4}>
+          <h3>Touch Screen</h3>
+          <FormControlLabel
+            control={
+              <Switch
+                color="secondary"
+                name="touch screen"
+                checked={newProduct.touchScreen}
+                onChange={() =>
+                  setNewProduct({
+                    ...newProduct,
+                    touchScreen: !newProduct.touchScreen,
+                  })
+                }
+              />
+            }
+            label="Product has this"
+          />
+        </Grid>
+
+        <Grid item xs={4}>
+          <h3>Magsafe</h3>
+          <FormControlLabel
+            control={
+              <Switch
+                name="magsafe"
+                color="secondary"
+                checked={newProduct.magsafe}
+                onChange={() =>
+                  setNewProduct({
+                    ...newProduct,
+                    magsafe: !newProduct.magsafe,
+                  })
+                }
+              />
+            }
+            label="Product has this"
+          />
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
